6844d50acc3165a921a214d28f92e6f94f277a74
DL <tiendung.work16@gmail.com>
2024-04-25 23:04:21 +0700

    day 8

---
+++ day8/index.js
@@ -0,0 +1,36 @@
+// 1
+// let i = 0;
+// let sum = 0;
+// while (i <= 100) {
+//   sum += i;
+//   i++;
+// }
+// console.log("Tong 1 den 100 :" + sum);
+
+// // 2
+
+// for (let index = 0; index <= 100; index++) {
+//   if (index % 3 == 0 && index % 5 == 0) {
+//     console.log(index);
+//   }
+// }
+
+// 3
+
+// let a = +prompt("Hay nhap so vao");
+// if (!isNaN(a)) {
+//   let b = 1;
+//   for (let index = 1; index <=3 a; index++) {
+//     b *= index;
+//   }
+//   console.log(b);
+// } else {
+//   console.log("hay f5 nhap lai");
+// }
+
+// 4
+// for (let a = 2; a <= 9; a++) {
+//   for (let x = 0; x <= 10; x++) {
+//     console.log(`${a} * ${x} = ${a * x}`);
+//   }
+// }
